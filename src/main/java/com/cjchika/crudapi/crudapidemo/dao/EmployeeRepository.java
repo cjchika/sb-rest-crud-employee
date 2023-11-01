5dc27ae7c5b3f86b9881df3a4b69b7a5f99df383
Chijioke Chika <8pluscreative@gmail.com>
2023-11-01 15:17:55 +0100

    Using Spring Data REST

--- src/main/java/com/cjchika/crudapi/crudapidemo/dao/EmployeeRepository.java
+++ src/main/java/com/cjchika/crudapi/crudapidemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cjchika.crudapi.crudapidemo.dao;
 
 import com.cjchika.crudapi.crudapidemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // NO NEED TO WRITE ANY CODE - WORKS OUT OF THE BOX
 }
